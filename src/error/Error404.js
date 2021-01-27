6f6ef5a97023fa500a399158a0f56fdae35d92fe
DIVYANG VIJAYVVARGIYA <divyangvijayvvargiya@DIVYANGs-MacBook-Pro.local>
2021-01-27 23:08:15 +0530

    previous project not working so pushed another one

---
+++ src/error/Error404.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react'
+
+export default class extends Component {
+
+  render() {
+    return (
+      <h1>Page not found</h1>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Controller from './screens/Controller';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Controller/>
   </React.StrictMode>,
   document.getElementById('root')
 );
